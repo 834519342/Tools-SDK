b4f338af3a7cd8fee4bd2049b22070a2ca5e4f52
TJ <834519342@qq.com>
2019-03-15 15:12:35 +0800

    .

--- TJTools/TJTools/TJTools.h
+++ TJTools/TJTools/TJTools.h
@@ -5,6 +5,18 @@
 //  Created by xt on 2018/10/30.
 //  Copyright © 2018年 TJ. All rights reserved.
 //
+/*
+ version 英 ['vɜːʃ(ə)n] n. 版本；译文；倒转术
+ crash 英 [kræʃ] n. 撞碎；坠毁；破产；轰隆声；睡觉
+ alert 英 [ə'lɜːt] n. 警戒，警惕；警报
+ tooltip 英 [tuːl] 英 [tɪp] n. 工具提示；提示信息；提示框；提示文本
+ local 英 ['ləʊk(ə)l] n. [计] 局部；当地居民；本地新闻
+ push 英 [pʊʃ] n. 推，决心；大规模攻势；矢志的追求
+ notification 英 [,nəʊtɪfɪ'keɪʃn] n. 通知；通告；[法] 告示
+ model 英 ['mɒdl] n. 模型；典型；模范；模特儿；样式
+ apple 英 ['æp(ə)l] n. 苹果
+ access 英 ['ækses] n. 进入；使用权；通路
+ */
 
 #import <UIKit/UIKit.h>
 #import "TJVersion.h"
