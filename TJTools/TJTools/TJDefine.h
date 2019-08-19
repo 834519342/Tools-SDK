07fadde340347325c53bf863d945298780d668b3
TJ <834519342@qq.com>
2019-08-19 14:01:24 +0800

    .

--- TJTools/TJTools/TJDefine.h
+++ TJTools/TJTools/TJDefine.h
@@ -11,11 +11,11 @@
  Xcode配置：Preprocessor Macros    DEBUG=1
  */
 #ifdef DEBUG
-# define TJLog(fmt, ...) NSLog((@"\n[文件：%s]\n" "[函数：%s]\n" "[行：%d]\n" "[日志：" fmt "]"),\
-[[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String],\
-__FUNCTION__,\
-__LINE__,\
-##__VA_ARGS__);
+    #define TJLog(fmt, ...) NSLog((@"\n[文件：%s]\n" "[函数：%s]\n" "[行：%d]\n" "[日志：" fmt "]"),\
+    [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String],\
+    __FUNCTION__,\
+    __LINE__,\
+    ##__VA_ARGS__);
 #else
-# define TJLog(fmt, ...) NSLog((fmt), ##__VA_ARGS__);
+    #define TJLog(fmt, ...) NSLog((fmt), ##__VA_ARGS__);
 #endif
